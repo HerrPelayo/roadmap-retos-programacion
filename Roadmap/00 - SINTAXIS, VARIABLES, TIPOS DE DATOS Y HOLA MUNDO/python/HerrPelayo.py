b12fad4aa807940062fc18fd11ad3daf7f878d88
HerrPelayo <dpeinadolopez@gmail.com>
2025-05-23 21:59:44 +0200

    Create HerrPelayo.py
    Ejercicio #0 SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/HerrPelayo.py
@@ -0,0 +1,19 @@
+# https://python.org
+
+# Comentario en una línea
+
+""" Esto es un 
+comentario
+en varias
+líneas """
+
+''' Que también
+puede realizarse
+con comillas simples '''
+
+mi_variable = "HerrPelayo"    # Estoy creando una variable de tipo string
+mi_numero = 10                # Estoy creando e inicializando otra variable de tipo entero.
+mi_numero_flotante = 10.5     # Estamos creando otra variable de tipo float, es decir, un número con decimales.
+
+MI_CONSTANTE = "Mi constante"  # Python no tiene constantes.
+
